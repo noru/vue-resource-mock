8b2e4ce3404c589dd8eb4f61c38f6d1fb16cb431
xiuxiuxiu <nooooru@gmail.com>
2016-11-30 14:24:34 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
-import UrlPattern from 'url-pattern';
+/** copy from https://github.com/noru/vue-resource-mock, since it is not registered on npm
+ */
+/* eslint-disable */
+import UrlPattern from 'url-pattern'
 
+const TAG = '[vue-resource-mock]'
+const MATCH_OPTIONS = {
+  segmentValueCharset: 'a-zA-Z0-9.:-_%'
+}
 /**
  * Route Map Creator
- * @param  {[type]} map [description]
- * @return {[type]}     [description]
+ * @param  {Object} map [description]
+ * @return {Array}     [description]
  */
 export const mapRoutes = (map) => {
-	// https://www.npmjs.com/package/url-pattern
-	return Object.keys(map)
-		.reduce((result, route) => {
-			let [method, url] = route.split(' '),
-				handler = routes[route];
-
-			if (handler.constructor.name === 'Function') {
-				result.concat([{
-					method: method,
-					pattern: new UrlPattern(url),
-					handler: handler
-				}]);
-			} else if (handler.constructor.name === 'Object') {
-				result.concat(mapRoutes(handler));
-			} else {
-				throw new Error('Routes must be an object or function');
-			}
-
-		}, []);
-};
+    // https://www.npmjs.com/package/url-pattern
+    return Object.keys(map)
+        .reduce((result, route) => {
+            let [method, url] = route.split(' '),
+                handler = map[route]
+            if (handler.constructor.name === 'Function') {
+                result = result.concat([{
+                    method: method,
+                    pattern: new UrlPattern(url, MATCH_OPTIONS),
+                    handler: handler
+                }])
+            } else if (handler.constructor.name === 'Object') {
+                result = result.concat(mapRoutes(handler))
+            } else {
+                throw new Error('Routes must be an object or function')
+            }
+            return result;
+        }, [])
+}
 
 /**
  * Interceptor Middleware
@@ -33,26 +39,22 @@ export const mapRoutes = (map) => {
  * @return {function}        middleware function
  */
 export const MockServer = (routes) => {
-	let Routes = mapRoutes(routes);
+    let Routes = mapRoutes(routes)
 
-	// https://github.com/vuejs/vue-resource/blob/master/docs/http.md#interceptors
-	return (request, next) => {
+    // https://github.com/vuejs/vue-resource/blob/master/docs/http.md#interceptors
+    return (request, next) => {
+        let route = Routes.filter((item) => {
+          item.matchResult = item.pattern.match(request.url.split('?')[0])
+          return request.method.toLowerCase() === item.method.toLowerCase() && !!item.matchResult
+        })
+        if (route.length == 0) {
+            console.warn(TAG + 'Request pass through: ' + request.url)
+            next()
+        } else {
+            console.info(TAG + 'Request served with mock: ' + request.url)
+            next(request.respondWith(route[0].handler(request, next, route[0].matchResult)))
+        }
 
-		let route = Routes.filter((item) => {
-			return request.method === item.method && !!item.pattern.match(request.url);
-		});
-
-		if (!route) {
-			next(request.respondWith({
-				status: 404,
-				statusText: 'Not Found',
-			}));
-		} else {
-			next(request.respondWith(route.handler(request, next)));
-		}
-
-		return;
-	};
+    }
 
 }
-
